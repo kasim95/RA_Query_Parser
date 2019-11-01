ba33fcdf3eedfe9bf6ae1326f9cfb2ffaaf4acdb
Kasim <panjrikasim@gmail.com>
2019-10-31 18:23:02 -0700

    Added Project and Select functions

--- queries.py
+++ queries.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
 import pprint
-
+import collections
 from Chinook_Python import *
 
 
 def project(relation, columns):
-    pass
+    # relation:  set
+    # columns: list of strings as column names
+    result = []
+    all_columns = list(relation)[0]._fields
+    tuple_ = collections.namedtuple("result", columns)
+    for i in range(len(relation)):
+        values = []
+        for j in range(len(columns)):
+            if columns[j] in all_columns:
+                values.append(list(relation)[i]._asdict()[columns[j]])
+        result.append(tuple_(*values))
+    result = set(result)
+    return result
 
 
 def select(relation, predicate):
-    pass
+    result = []
+    all_columns = list(relation)[0]._fields
+    tuple_ = collections.namedtuple("result", all_columns)
+    for i in range(len(relation)):
+        if predicate(list(relation)[i]):
+            values = []
+            for j in all_columns:
+                values.append(list(relation)[i]._asdict()[j])
+            result.append(tuple_(*values))
+    result = set(result)
+    return result
 
 
 def rename(relation, new_columns=None, new_relation=None):
@@ -28,7 +50,7 @@ def theta_join(relation1, relation2, predicate):
 def natural_join(relation1, relation2):
     pass
 
-
+"""
 pprint.pprint(
     project(
         select(
@@ -82,3 +104,8 @@ pprint.pprint(
         ['Title']
     )
 )
+"""
+
+
+pprint.pprint(project(Artist, ["Name"]))
+pprint.pprint(select(Artist, lambda t: t.Name == "Red Hot Chili Peppers"))
