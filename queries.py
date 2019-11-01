914ceb8bdd044fbcf97159f366521e2c400d56e2
Kasim <panjrikasim@gmail.com>
2019-11-01 02:23:08 -0700

    Added cross

--- queries.py
+++ queries.py
@@ -34,8 +34,26 @@ def rename(relation, new_columns=None, new_relation=None):
         result.append(tuple_(*values))
     return set(result)
 
+
 def cross(relation1, relation2):
-    pass
+    rel1_columns = list(next(iter(relation1))._fields)
+    rel2_columns = list(next(iter(relation2))._fields)
+    rel1_name = next(iter(relation1)).__repr__().split("(")[0]
+    rel2_name = next(iter(relation2)).__repr__().split("(")[0]
+    # rename same column names
+    new_rel1_columns = rel1_columns.copy()
+    new_rel2_columns = rel2_columns.copy()
+    new_rel1_columns = [(rel1_name + "_" + new_rel1_columns[i]) if (new_rel1_columns[i] in rel2_columns) else new_rel1_columns[i] for i in range(len(new_rel1_columns))]
+    new_rel2_columns = [(rel2_name + "_" + new_rel2_columns[i]) if (new_rel2_columns[i] in rel1_columns) else new_rel2_columns[i] for i in range(len(new_rel2_columns))]
+
+    tuple_ = collections.namedtuple("result", new_rel1_columns+new_rel2_columns)
+    result = []
+    for i in range(len(relation1)):
+        for j in range(len(relation2)):
+            values1 = list(list(relation1)[i].__getnewargs__())
+            values2 = list(list(relation2)[j].__getnewargs__())
+            result.append(tuple_(*(values1 + values2)))
+    return set(result)
 
 
 def theta_join(relation1, relation2, predicate):
@@ -44,6 +62,7 @@ def theta_join(relation1, relation2, predicate):
 
 def natural_join(relation1, relation2):
     pass
+
 
 """
 pprint.pprint(
@@ -105,3 +124,4 @@ pprint.pprint(
 pprint.pprint(project(Artist, ["Name"]))
 pprint.pprint(select(Artist, lambda t: t.Name == "Red Hot Chili Peppers"))
 pprint.pprint(rename(Album,["Id", "NameofAlbum", "Artist"]))
+pprint.pprint(len(cross(Artist, Album)))
