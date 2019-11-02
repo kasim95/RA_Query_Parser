d2e820d72369a48dbd147113ff6d103e9cb3c3c4
Kasim <panjrikasim@gmail.com>
2019-11-02 13:45:29 -0700

    Added natural join and changed variable names

--- queries.py
+++ queries.py
@@ -28,7 +28,7 @@ def rename(relation, new_columns=None, new_relation=None):
     result = []
     all_columns = list(next(iter(relation))._asdict().keys())
     tuple_name = next(iter(relation)).__repr__().split("(")[0]
-    tuple_ = collections.namedtuple(tuple_name,new_columns)
+    tuple_ = collections.namedtuple(tuple_name, new_columns)
     iter_ = iter(relation)
     for i in range(len(relation)):
         record = next(iter_)
@@ -42,47 +42,70 @@ def cross(relation1, relation2):
     rel2_columns = list(next(iter(relation2))._fields)
     rel1_name = next(iter(relation1)).__repr__().split("(")[0]
     rel2_name = next(iter(relation2)).__repr__().split("(")[0]
-    # rename same column names
+    #rename same column names
     new_rel1_columns = rel1_columns.copy()
     new_rel2_columns = rel2_columns.copy()
     new_rel1_columns = [(rel1_name + "_" + new_rel1_columns[i]) if (new_rel1_columns[i] in rel2_columns) else new_rel1_columns[i] for i in range(len(new_rel1_columns))]
     new_rel2_columns = [(rel2_name + "_" + new_rel2_columns[i]) if (new_rel2_columns[i] in rel1_columns) else new_rel2_columns[i] for i in range(len(new_rel2_columns))]
-    tuple_ = collections.namedtuple("result", new_rel1_columns+new_rel2_columns)
+    tuple_ = collections.namedtuple("result", new_rel1_columns + new_rel2_columns)
     result = []
     iter1 = iter(relation1)
     for i in range(len(relation1)):
-        tuple1 = next(iter1)
-        values1 = list(tuple1.__getnewargs__())
+        record1 = next(iter1)
+        values1 = list(record1.__getnewargs__())
         iter2 = iter(relation2)
         for j in range(len(relation2)):
-            tuple2 = next(iter2)
-            values2 = list(tuple2.__getnewargs__())
-            result.append(tuple_(*(values1+ values2)))
+            record2 = next(iter2)
+            values2 = list(record2.__getnewargs__())
+            result.append(tuple_(*(values1 + values2)))
     return set(result)
 
 
 def theta_join(relation1, relation2, predicate):
     rel1_columns = list(next(iter(relation1))._fields)
     rel2_columns = list(next(iter(relation2))._fields)
-    tuple_ = collections.namedtuple("result", rel1_columns+rel2_columns)
+    tuple_ = collections.namedtuple("result", rel1_columns + rel2_columns)
     result = []
     iter1 = iter(relation1)
     for i in range(len(relation1)):
-        dict1 = next(iter1)
+        record1 = next(iter1)
         iter2 = iter(relation2)
-        result.append(dict1._asdict())
         for j in range(len(relation2)):
-            dict2 = next(iter2)
-            keys2 = list(dict2._asdict())
-            if predicate(dict1, dict2):
+            record2 = next(iter2)
+            if predicate(record1, record2):
+                resultdict = record1._asdict()
                 for k in rel2_columns:
-                    result[i][k] = dict2._asdict()[k]
-        result[i] = tuple_(**result[i])
+                    resultdict[k] = record2._asdict()[k]
+                result.append(tuple_(**resultdict))
+                break;
     return set(result)
 
 
 def natural_join(relation1, relation2):
-    pass
+    rel1_columns = list(next(iter(relation1))._fields)
+    rel2_columns = list(next(iter(relation2))._fields)
+    common_col = None
+    for i in rel1_columns:
+        if i in rel2_columns:
+            common_col = i
+    if (common_col == None):
+        raise ValueError("No common Column found")
+    all_columns = list(set(rel1_columns + rel2_columns))
+    tuple_ = collections.namedtuple("result", all_columns)
+    result = []
+    iter1 = iter(relation1)
+    for i in range(len(relation1)):
+        record1 = next(iter1)
+        iter2 = iter(relation2)
+        for j in range(len(relation2)):
+            record2 = next(iter2)
+            if record1._asdict()[common_col] == record2._asdict()[common_col]:
+                resultdict = record1._asdict()
+                for k in rel2_columns:
+                    if k != common_col:
+                        resultdict[k] = record2._asdict()[k]
+                result.append(tuple_(**resultdict))
+    return set(result)
 
 
 """
@@ -101,7 +124,7 @@ pprint.pprint(
         ['Title']
     )
 )
-
+"""
 pprint.pprint(
     project(
         select(
@@ -139,11 +162,3 @@ pprint.pprint(
         ['Title']
     )
 )
-"""
-
-
-pprint.pprint(project(Artist, ["Name"]))
-pprint.pprint(select(Artist, lambda t: t.Name == "Red Hot Chili Peppers"))
-pprint.pprint(rename(Album,["Id", "NameofAlbum", "Artist"]))
-pprint.pprint(len(cross(Artist, Album)))
-pprint.pprint(theta_join(Album, rename(Artist, ['Id', 'Name']), lambda t1, t2: t1.ArtistId == t2.Id))
