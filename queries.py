372fdf645cfe10d04888227871b2ae02bc70ae56
Kasim <panjrikasim@gmail.com>
2019-11-05 15:50:16 -0800

    Updated queries for performance optimization and added query 5

--- queries.py
+++ queries.py
@@ -2,25 +2,39 @@
 
 import pprint
 import collections
+import math
 from Chinook_Python import *
 
 
 def project(relation, columns):
-    result = []
     all_columns = next(iter(relation))._fields
     tuple_name = next(iter(relation)).__repr__().split("(")[0]
     tuple_ = collections.namedtuple(tuple_name, columns)
     iter_ = iter(relation)
+    iterations = math.ceil(len(relation)/10000)
+    result_list = [[] for i in range(iterations)]
     for i in range(len(relation)):
         #get the values for each column field
         record = next(iter_)
         values = [record._asdict()[x] for x in columns if x in all_columns]
-        result.append(tuple_(*values))
+        result_list[math.floor(i/10000)].append(tuple_(*values))
+    result = [k for m in result_list for k in m]
+    print("Cardinality of project: ",len(result))
     return set(result)
 
 
 def select(relation, predicate):
-    result = [list(relation)[i] for i in range(len(relation)) if predicate(list(relation)[i])]
+    iterations = math.ceil(len(relation) / 10000)
+    rel_batch_list = []
+    result_list = []
+    for i in range(iterations):
+        start = i * 10000
+        end = (i + 1) * 10000
+        rel_batch_list = list(relation)[start:end]
+        result_list.append([j for j in rel_batch_list if predicate(j)])
+
+    result = [k for m in result_list for k in m]
+    print("Cardinality of select: ", len(result))
     return set(result)
 
 
@@ -30,13 +44,17 @@ def rename(relation, new_columns=None, new_relation=None):
     tuple_name = next(iter(relation)).__repr__().split("(")[0]
     tuple_ = collections.namedtuple(tuple_name, new_columns)
     iter_ = iter(relation)
+    iterations = math.ceil(len(relation)/10000)
+    result_list = [[] for i in range(iterations)]
     for i in range(len(relation)):
         record = next(iter_)
         values = [record._asdict()[j] for j in all_columns]
-        result.append(tuple_(*values))
+        result_list[math.floor(i/10000)].append(tuple_(*values))
+    result = [k for m in result_list for k in m]
+    print("Cardinality of rename: ",len(result))
     return set(result)
 
-
+#still the fastest of them all
 def cross(relation1, relation2):
     rel1_columns = list(next(iter(relation1))._fields)
     rel2_columns = list(next(iter(relation2))._fields)
@@ -48,7 +66,8 @@ def cross(relation1, relation2):
     new_rel1_columns = [(rel1_name + "_" + new_rel1_columns[i]) if (new_rel1_columns[i] in rel2_columns) else new_rel1_columns[i] for i in range(len(new_rel1_columns))]
     new_rel2_columns = [(rel2_name + "_" + new_rel2_columns[i]) if (new_rel2_columns[i] in rel1_columns) else new_rel2_columns[i] for i in range(len(new_rel2_columns))]
     tuple_ = collections.namedtuple("result", new_rel1_columns + new_rel2_columns)
-    result = []
+    iterations = math.ceil((len(relation1) * len(relation2)) / 10000)
+    result_list = [[] for i in range(iterations)]
     iter1 = iter(relation1)
     for i in range(len(relation1)):
         record1 = next(iter1)
@@ -57,7 +76,9 @@ def cross(relation1, relation2):
         for j in range(len(relation2)):
             record2 = next(iter2)
             values2 = list(record2.__getnewargs__())
-            result.append(tuple_(*(values1 + values2)))
+            result_list[math.floor(i / 10000)].append(tuple_(*(values1 + values2)))
+    result = [k for m in result_list for k in m]
+    print("Cardinality of cross: ",len(result))
     return set(result)
 
 
@@ -65,7 +86,9 @@ def theta_join(relation1, relation2, predicate):
     rel1_columns = list(next(iter(relation1))._fields)
     rel2_columns = list(next(iter(relation2))._fields)
     tuple_ = collections.namedtuple("result", rel1_columns + rel2_columns)
-    result = []
+    #result = []
+    iterations = math.ceil((len(relation1) * len(relation2)) / 10000)
+    result_list = [[] for i in range(iterations)]
     iter1 = iter(relation1)
     for i in range(len(relation1)):
         record1 = next(iter1)
@@ -76,8 +99,10 @@ def theta_join(relation1, relation2, predicate):
                 resultdict = record1._asdict()
                 for k in rel2_columns:
                     resultdict[k] = record2._asdict()[k]
-                result.append(tuple_(**resultdict))
+                result_list[math.floor(i / 10000)].append(tuple_(**resultdict))
                 break;
+    result = [k for m in result_list for k in m]
+    print("Cardinality of theta join: ",len(result))
     return set(result)
 
 
@@ -92,7 +117,9 @@ def natural_join(relation1, relation2):
         raise ValueError("No common Column found")
     all_columns = list(set(rel1_columns + rel2_columns))
     tuple_ = collections.namedtuple("result", all_columns)
-    result = []
+    #result = []
+    iterations = math.ceil((len(relation1) * len(relation2)) / 10000)
+    result_list = [[] for i in range(iterations)]
     iter1 = iter(relation1)
     for i in range(len(relation1)):
         record1 = next(iter1)
@@ -104,11 +131,13 @@ def natural_join(relation1, relation2):
                 for k in rel2_columns:
                     if k != common_col:
                         resultdict[k] = record2._asdict()[k]
-                result.append(tuple_(**resultdict))
+                #result.append(tuple_(**resultdict))
+                result_list[math.floor(i / 10000)].append(tuple_(**resultdict))
+    result = [k for m in result_list for k in m]
+    print("Cardinality of natural join: ",len(result))
     return set(result)
 
-
-"""
+print("\n\nQUERY 1\n")
 pprint.pprint(
     project(
         select(
@@ -124,7 +153,8 @@ pprint.pprint(
         ['Title']
     )
 )
-"""
+
+print("\n\nQUERY 2\n")
 pprint.pprint(
     project(
         select(
@@ -139,6 +169,7 @@ pprint.pprint(
     )
 )
 
+print("\n\nQUERY 3\n")
 pprint.pprint(
     project(
         theta_join(
@@ -153,6 +184,8 @@ pprint.pprint(
     )
 )
 
+print("\n\nQUERY 4\n")
+
 pprint.pprint(
     project(
         natural_join(
@@ -162,3 +195,13 @@ pprint.pprint(
         ['Title']
     )
 )
+
+
+#OLD QUERY
+print("\n\nQUERY 5 (Project 1 query 1)\n")
+pprint.pprint(project(select(natural_join(Album, Artist), lambda t: t.Name == 'Red Hot Chili Peppers'),['Title']))
+
+
+#NEW QUERY
+print("\n\nOptimized QUERY 5 (Project 1 query 1)\n")
+pprint.pprint(project(natural_join(Album, select(Artist, lambda t: t.Name == 'Red Hot Chili Peppers')), ['Title']))
